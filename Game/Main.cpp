9be691c0897351b6862fc846d1a8f7e8bd8645e7
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2020-08-04 15:14:08 -0600

    texture bmp load

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -10,14 +10,79 @@ int main(int, char**)
 	}
 
 	SDL_Window* window = SDL_CreateWindow("GAT150", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+	if (window == nullptr) 
+	{
+		std::cout << "Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
 
-	// wait for keyboard enter to exit
-	std::getchar();
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr)
+	{
+		std::cout << "Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	int width = 128; // 32, 64, 128, 256, 512, 1024, 2048, 4096
+	int height = 128;
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, width, height);
+	Uint32* pixels = new Uint32[width * height]; // Unit32 (r,g,b,a) (8,8,8,8)
+	memset(pixels, 255, width * height * sizeof(Uint32)); // (255, 255, 255, 255) (255, 255, 255, 255)
+	SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(Uint32));
+
+	SDL_Surface* surface = SDL_LoadBMP("sf2.bmp");
+	if (surface == nullptr)
+	{
+		std::cout << "Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, surface);
+
+	SDL_Event event;
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+		SDL_RenderClear(renderer);
+
+		// draw
+
+		for (int i = 0; i < width * height; i++)
+		{
+			Uint8 c1 = rand() % 256;
+			Uint8 c2 = rand() % 256;
+			Uint8 c3 = rand() % 256;
+			pixels[i] = (c1 << 24 | c2 << 16 | c3 << 8); // (r, g, b, a) (8, 8, 8, 8)
+		}
+		SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(Uint32));
+
+		SDL_Rect rect;
+		rect.x = 140;
+		rect.y = 140;
+		rect.w = width;
+		rect.h = height;
+		SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+
+		SDL_Rect rect2;
+		rect2.x = 20;
+		rect2.y = 20;
+		SDL_QueryTexture(texture2, NULL, NULL, &rect2.w, &rect2.h);
+		SDL_RenderCopy(renderer, texture2, NULL, &rect2);
+
+		SDL_RenderPresent(renderer);
+	}
 
 	SDL_Quit();
 
