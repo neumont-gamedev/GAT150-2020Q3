b8a93ab2012f933472412951d3ddcdcad5d4bd55
neumont-gamedev <rmaple@neumont.edu>
2020-08-20 16:12:36 -0600

    sprite animation sprite origin

--- Engine/Objects/ObjectFactory.cpp
+++ Engine/Objects/ObjectFactory.cpp
@@ -2,6 +2,7 @@
 #include "ObjectFactory.h"
 #include "Components/PhysicsComponent.h"
 #include "Components/SpriteComponent.h"
+#include "Components/SpriteAnimationComponent.h"
 
 namespace nc
 {
@@ -10,5 +11,6 @@ namespace nc
 		nc::ObjectFactory::Instance().Register("GameObject", nc::Object::Instantiate<nc::GameObject>);
 		nc::ObjectFactory::Instance().Register("PhysicsComponent", nc::Object::Instantiate<nc::PhysicsComponent>);
 		nc::ObjectFactory::Instance().Register("SpriteComponent", nc::Object::Instantiate<nc::SpriteComponent>);
+		nc::ObjectFactory::Instance().Register("SpriteAnimationComponent", nc::Object::Instantiate<nc::SpriteAnimationComponent>);
 	}
 }
